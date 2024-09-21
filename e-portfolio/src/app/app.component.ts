942f5920e33472133050dc9e05cbb9643ef1927a
Johnstanley Ajagu <johnstanley.ajagu@gmail.com>
2024-09-20 22:56:54 -0400

    Added home page component and updated routing

--- e-portfolio/src/app/app.component.ts
+++ e-portfolio/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HomePageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'e-portfolio';
+  
 }
