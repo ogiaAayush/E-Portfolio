bcd675ae6f1fb87beecd76b5aafbbfae584657d0
Johnstanley Ajagu <johnstanley.ajagu@gmail.com>
2024-09-22 16:27:08 -0400

    added nav component

--- e-portfolio/src/app/app.component.ts
+++ e-portfolio/src/app/app.component.ts
@@ -2,14 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './home-page/home-page.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HomePageComponent],
+  imports: [CommonModule, RouterOutlet, HomePageComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  
+
 }
