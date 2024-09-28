33ea587fbc578d7e2ab8ff59af2c857527d54d5a
Johnstanley Ajagu <johnstanley.ajagu@gmail.com>
2024-09-27 23:52:18 -0400

    Fixed animation

--- e-portfolio/src/app/home-page/home-page.component.ts
+++ e-portfolio/src/app/home-page/home-page.component.ts
@@ -15,25 +15,51 @@ import { CommonModule } from '@angular/common';
       state('out', style({ opacity: 1, transform: 'translateY(20px)' })),
       transition('out => in', [
         style({ opacity: 0, transform: 'translateY(20px)' }),
-        animate('1000ms ease-out')
+        animate('600ms ease-out')
       ]),
       transition('in => out', [
-        animate('1000ms ease-in', style({ opacity: 0, transform: 'translateY(20px)' }))
+        animate('600ms ease-in', style({ opacity: 0, transform: 'translateY(20px)' }))
       ])
     ])
   ]
 })
+
+
 export class HomePageComponent {
   title: string = "e-portfolio";
+
+  private isThrottled = false; // Flag to prevent more than 1 scroll at a time before the initial scrolls completes
+
 
   sections = ['section1', 'section2', 'section3', 'section4'];
   sectionStates: { [key: string]: string } = { section1: 'out', section2: 'out', section3: 'out', section4: 'out' }
 
   // Listen for window scroll event
   @HostListener('window:scroll', ['$event'])
-  onscroll() {
+  onScroll() {
     this.checkScroll();
   }
+
+  @HostListener('window:wheel', ['$event'])
+  onScrollFocus(event: WheelEvent): void {
+    if(this.isThrottled) {
+      return; // Ignore scroll if one is still in progress
+    }
+
+    this.isThrottled = true;
+
+    if (event.deltaY > 0) {
+      // Scroll down by one full view port height
+      window.scrollBy({ top: window.innerHeight, left: 0, behavior: 'smooth' });
+    } else {
+      window.scrollBy({top: -window.innerHeight, left: 0, behavior: 'smooth'})
+    }
+
+    setTimeout(() => {
+      this.isThrottled = false
+    }, 1000);
+  }
+
 
   checkScroll() {
     const section1 = document.getElementById('section1') as HTMLElement | null;
@@ -47,8 +73,6 @@ export class HomePageComponent {
       if (section) {
         const binding = section.getBoundingClientRect();
         const sectionName = `section${index + 1}`;
-
-        // console.log(`section ${sectionName} binding.top:`, binding.top)
 
         if (binding.top >= 0 && binding.bottom <= (window.innerHeight || document.documentElement.clientHeight)) {
           this.sectionStates[sectionName] = 'in'; // Fade in
