d8c37e46514eb24036fb0e10490f32078a5aed9a
Johnstanley Ajagu <johnstanley.ajagu@gmail.com>
2024-09-29 01:17:46 -0400

    Updated UI

--- e-portfolio/src/app/navbar/navbar.component.ts
+++ e-portfolio/src/app/navbar/navbar.component.ts
@@ -4,10 +4,9 @@ import { RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
 }
