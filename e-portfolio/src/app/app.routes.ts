3f99425452cdb873502907a9342d32fb0f08c6ca
Johnstanley Ajagu <johnstanley.ajagu@gmail.com>
2024-09-29 00:33:21 -0400

    added certificate & experience nav

--- e-portfolio/src/app/app.routes.ts
+++ e-portfolio/src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { CertificateComponent } from './certificate/certificate.component';
 
 export const routes: Routes = [
-    {path: "home", component: HomePageComponent},
-    {path: "", redirectTo: "/home", pathMatch: "full"}
+    { path: "home", component: HomePageComponent },
+    { path: "experience", component: ExperienceComponent },
+    { path: "certificate0", component: CertificateComponent },
+    { path: "", redirectTo: "/home", pathMatch: "full" }
 ];
