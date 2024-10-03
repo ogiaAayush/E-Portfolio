884508deecf086238a8f2338c1cbeaf16420b239
Johnstanley Ajagu <johnstanley.ajagu@gmail.com>
2024-10-02 23:08:43 -0400

    Ui Update

--- e-portfolio/src/app/app.routes.ts
+++ e-portfolio/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { ExperienceComponent } from './experience/experience.component';
 import { CertificateComponent } from './certificate/certificate.component';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
     { path: "home", component: HomePageComponent },
+    { path: "about", component: AboutComponent },
     { path: "experience", component: ExperienceComponent },
     { path: "certificate", component: CertificateComponent },
     { path: "", redirectTo: "/home", pathMatch: "full" }
