0b956a073e6aef08118b5f835b07c14dc6822991
Johnstanley Ajagu <johnstanley.ajagu@gmail.com>
2024-10-09 21:18:44 -0400

    created projects page

--- e-portfolio/src/app/app.routes.ts
+++ e-portfolio/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { ExperienceComponent } from './experience/experience.component';
 import { CertificateComponent } from './certificate/certificate.component';
 import { AboutComponent } from './about/about.component';
+import { ProjectComponent } from './project/project.component';
 
 export const routes: Routes = [
     { path: "home", component: HomePageComponent },
     { path: "about", component: AboutComponent },
     { path: "experience", component: ExperienceComponent },
     { path: "certificate", component: CertificateComponent },
+    { path: "project", component: ProjectComponent },
     { path: "", redirectTo: "/home", pathMatch: "full" }
 ];
